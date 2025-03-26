31637dbff4fc578163a8b9550071606ba90d860d
Khan B <itisameerkhan@gmail.com>
2025-03-26 20:21:37 +0530

    error page

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -8,7 +8,7 @@ const Error = () => {
     <div className="error-element">
       <Header />
       <div className="error-element-main">
-        <h1>INVALID PATH</h1>
+        <h1>SOMETHING WENT WRONG</h1>
         <h2>404 Not Found</h2>
         <Link to={"/"}>
           <Button variant="outlined">Home</Button>
